e62a900be8005b6b86e9034ba656240f50df346f
ejgandelaberon <ejgandelaberon@yahoo.com>
2022-05-11 22:22:02 +0800

    add auth service to login

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { PrimeNGConfig } from 'primeng/api';
+import { AuthService } from '../../service/auth.service';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -13,7 +16,10 @@ export class LoginPageComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private primengConfig: PrimeNGConfig
+    private primengConfig: PrimeNGConfig,
+    private authService: AuthService,
+    private messageService: MessageService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -30,8 +36,20 @@ export class LoginPageComponent implements OnInit {
 
   login(){
     if (this.loginForm.valid) {
-      console.log(this.loginForm.value)
-      this.loginForm.reset();
+      this.authService.login(this.loginForm.value).subscribe({
+        next: res => {
+          this.messageService.add({severity:'success', summary:'Verified', detail:res.message})
+          localStorage.setItem('token', res.accessToken)
+          this.router.navigate(['admin'])
+        },
+        error: err => {
+          this.messageService.add({severity:'error', summary:'Error', detail:err.error.message})
+        }
+      })
+      this.loginForm.reset()
+    } else {
+      if (!this.loginForm.value.username) return this.messageService.add({severity:'error', summary:'Empty username', detail:'Username is required'})
+      if (!this.loginForm.value.password) return this.messageService.add({severity:'error', summary:'Empty password', detail:'Password is required'})
     }
   }
 
