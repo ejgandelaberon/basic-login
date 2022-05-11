d93bbf00ce8bf0d7902d306843219e8a9cbf3cac
ejgandelaberon <ejgandelaberon@yahoo.com>
2022-05-11 22:24:06 +0800

    add role and auth guards to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,22 @@
+import { RoleGuard } from './shared/role.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './pages/admin/admin.component';
 import { UserAccountComponent } from './pages/admin/contents/user-account/user-account.component';
 import { UsersComponent } from './pages/admin/contents/users/users.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { AuthGuard } from './shared/auth.guard';
 
 const routes: Routes = [
-  {path:'', redirectTo:'login', pathMatch:'full'},
+  {path:'', redirectTo:'admin/users', pathMatch:'full'},
   {path:'admin', redirectTo:'admin/users', pathMatch:'full'},
   {path:'login', component:LoginPageComponent},
   {path:'admin', component:AdminComponent,
     children:[
       {path:'users', component:UsersComponent},
-      {path:'user-account', component:UserAccountComponent}
-    ]
+      {path:'user-account', component:UserAccountComponent, canActivate:[RoleGuard]}
+    ],
+    canActivate:[AuthGuard]
   }
 ];
 
